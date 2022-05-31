e148da2d434f198053e7f6c98998d4cabc3792ef
37256 <37256@MSI>
2022-05-31 20:27:31 +0200

    Добавьте файлы проекта.

---
+++ sv/sv/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace sv
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            drList.ItemsSourse = App.DataBase.GetItems();
+            base.OnAppearing();
+        }
+        private async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Dr selectedDr
+        }
+
+    }
+}
